625514121f30f49720447850b4e2fe5c05a540d1
Jacob Burch <jacob@jbbookpro.local>
2009-07-08 03:23:46 -0700

    Few bug fixes

--- prowlpy.py
+++ prowlpy.py
@@ -12,13 +12,12 @@ __version__ = 0.3
 import httplib2
 import urllib
 
+API_DOMAIN = 'https://prowl.weks.net/publicapi'
 
-
-API_DOMAIN = 'https://prowl.weks.net/publicapi/add'
 class Prowl(object):
     def __init__(self,apikey,username=None,password=None):
         self.apikey = apikey
-        self.username = username #currently not in use
+        self.username = username #Currently not in use
         self.password = password #Currently not in use
         
     def post(self,application=None,event=None,description=None):
@@ -32,15 +31,23 @@ class Prowl(object):
         # URL-encode and string-ify keywords. Better type/content testing is needed here
         application = urllib.quote(str(application))
         event = urllib.quote(str(event))
-        desciption = urllib.quote(str(description))
+        description = urllib.quote(str(description))
         
-        # Perform the request and get the response headers and content (Content should be blank for now)
-        resp,content = h.request("%s?application=%s&event=%s&description=%s&apikey=%s" \
+        verify_resp,verify_content = h.request("%s/verify?apikey=%s" % \
+                                                    (API_DOMAIN,self.apikey))
+        
+        if verify_resp['status']!='200':
+            raise Exception("Invalid API Key %s" % verify_content)
+        
+        # Perform the request and get the response headers and content
+        resp,content = h.request("%s/add?application=%s&event=%s&description=%s&apikey=%s" \
                                     % (API_DOMAIN,application, event, description,self.apikey))
+        url = "%s/add?application=%s&event=%s&description=%s&apikey=%s" \
+                % (API_DOMAIN,application, event, description,self.apikey)
         
-        if(resp['status']=='200'):
+        if resp['status']=='200':
             return True
-        elif(resp['status']=='401'): 
-            raise Exception("Auth Failed")
+        elif resp['status']=='401': 
+            raise Exception("Auth Failed %s" % url)
         else:
             raise Exception("Failed")
