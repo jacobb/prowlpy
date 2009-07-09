13e7a37bcea680fd6e51b212bed8254af1cb5f60
Jacob Burch <jacob@jbiMac.local>
2009-07-09 16:00:08 -0700

    V0.4

--- prowlpy.py
+++ prowlpy.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
 """
-Prowlpy V0.3
+Prowlpy V0.4
 
 Written by Jacob Burch, 7/6/2009
 
 Python module for posting to the iPhone Push Notification service Prowl: http://prowl.weks.net/
 """
 __author__ = 'jacobburch@gmail.com'
-__version__ = 0.3
+__version__ = 0.4
 
 import httplib2
 import urllib
 
+httplib2.debuglevel = 4
 API_DOMAIN = 'https://prowl.weks.net/publicapi'
 
 class Prowl(object):
@@ -21,9 +22,9 @@ class Prowl(object):
         self.password = password #Currently not in use
         
     def post(self, application=None, event=None, description=None):
+        
         # Create the http object
         h = httplib2.Http(".cache")
-        #h.add_credentials(self.username,self.password)
         
         # Set User-Agent
         headers = {'User-Agent': "Prowlpy/%s" % str(__version__)}
@@ -33,21 +34,37 @@ class Prowl(object):
         event = urllib.quote(str(event))
         description = urllib.quote(str(description))
         
-        verify_resp,verify_content = h.request("%s/verify?apikey=%s" % \
-                                                    (API_DOMAIN,self.apikey))
-        
-        if verify_resp['status'] != '200':
-            raise Exception("Invalid API Key %s" % verify_content)
         
         # Perform the request and get the response headers and content
-        resp,content = h.request("%s/add?application=%s&event=%s&description=%s&apikey=%s" \
-                                    % (API_DOMAIN,application, event, description,self.apikey))
-        url = "%s/add?application=%s&event=%s&description=%s&apikey=%s" \
-                % (API_DOMAIN,application, event, description,self.apikey)
+        data = {
+            'application': application,
+            'event': event,
+            'description': description,
+            'apikey': self.apikey
+        }
+        headers["Content-type"] = "application/x-www-form-urlencoded"
         
+        resp,content = h.request("%s/add/" % API_DOMAIN, "POST", headers=headers, body=urllib.urlencode(data))
         if resp['status'] == '200':
             return True
         elif resp['status'] == '401': 
-            raise Exception("Auth Failed %s" % url)
+            raise Exception("Auth Failed: %s" % content)
         else:
             raise Exception("Failed")
+        
+    def add(self, application=None, event=None, description=None):
+        """
+        Alias for the post function
+        Will become the default namespace for adding events by version 1.0
+        """
+        return self.post(application=application,event=event,description=description)
+    
+    def verify_key(self):
+        h = httplib2.Http(".cache")
+        headers = {'User-Agent': "Prowlpy/%s" % str(__version__)}
+        verify_resp,verify_content = h.request("%s/verify?apikey=%s" % \
+                                                    (API_DOMAIN,self.apikey))
+        if verify_resp['status'] != '200':
+            raise Exception("Invalid API Key %s" % verify_content)
+        else:
+            return True
